17fc1148784e3049d5e9318de7c233423086d510
Jose Perez <joseperez@MacBook-Pro-de-Jose.local>
2026-05-31 16:22:47 -0600

    Initial commit - Constant, Network and User Defualts created

---
+++ BlackSpartan/BlackSpartan.docc/BlackSpartan.md
@@ -0,0 +1,13 @@
+# ``BlackSpartan``
+
+Framework to work for a network layer of UFC Predict APP
+
+## Overview
+
+<!--@START_MENU_TOKEN@-->Text<!--@END_MENU_TOKEN@-->
+
+## Topics
+
+### <!--@START_MENU_TOKEN@-->Group<!--@END_MENU_TOKEN@-->
+
+- <!--@START_MENU_TOKEN@-->``Symbol``<!--@END_MENU_TOKEN@-->
---
+++ BlackSpartan/BlackSpartan.h
@@ -0,0 +1,18 @@
+//
+//  BlackSpartan.h
+//  BlackSpartan
+//
+//  Created by Jose Perez on 31/05/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlackSpartan.
+FOUNDATION_EXPORT double BlackSpartanVersionNumber;
+
+//! Project version string for BlackSpartan.
+FOUNDATION_EXPORT const unsigned char BlackSpartanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlackSpartan/PublicHeader.h>
+
+
